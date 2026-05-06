22c5e232d2d2b75c3ec909cf5ecdb3c4cc0f29e3
wulf6 <p.nelhybel@seznam.cz>
2026-05-06 23:48:55 +0200

    Delete Script.js

--- Script.js
+++
@@ -1,5 +0,0 @@
-fetch('products.json').then(r=>r.json()).then(products=>{
-  const grid = document.querySelector('#products-grid');
-  if(!grid) return;
-  grid.innerHTML = products.map(p=>`<article class="product"><div class="fav">♡</div><img src="${p.image}" alt="${p.name}"><div class="content"><p class="title">${p.name}</p><p class="price">${p.price}</p><a class="cart" href="#">Do košíku</a></div></article>`).join('');
-});
